d4b97947d1f6b0b1a9ea1db16770b191b6545588
Ak Noob Kutty <aknoobkutty@gmail.com>
2025-02-18 22:08:27 +0530

    comit

--- assets/UI-Components-main/Menus/01/script.js
+++
@@ -1,5 +0,0 @@
-const menuBtn = document.querySelector(".menu-btn");
-
-menuBtn.addEventListener("click", () => {
-  document.body.classList.toggle("open");
-});
--- assets/UI-Components-main/Menus/03/script.js
+++
@@ -1,8 +0,0 @@
-const menuBtn = document.querySelector(".menu-btn");
-const wrapper = document.querySelector(".wrapper");
-
-menuBtn.addEventListener("click", function () {
-  menuBtn.querySelector("i").classList.toggle("fa-xmark");
-
-  wrapper.classList.toggle("open");
-});
